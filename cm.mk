8c26890f7f8c0e0999afaec79477e7d9edc0ae2d
deadman96385 <seanhoyt963@gmail.com>
2016-11-10 23:29:57 -0600

    Import ril
    Change-Id: Ib5d489d60851b27785c296be5c4489789c8b21e9

--- cm.mk
+++ cm.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/samsung/v1a3g/full_v1a3g.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
+# Inherit more cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/telephony.mk)
+
 PRODUCT_NAME := cm_v1a3g
 PRODUCT_DEVICE := v1a3g
 
