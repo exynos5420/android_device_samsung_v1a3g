16dafd461a66448ac9210670d347b5c6bcd73d23
Martin Bouchet <tincho5588@gmail.com>
2016-12-08 21:12:22 -0600

    Define the right modules for Radio in device.mk
    Change-Id: Ia49ce5b1c520b306e82c9277055bf6439c5454bc

--- device.mk
+++ device.mk
@@ -181,11 +181,10 @@ PRODUCT_PACKAGES += \
 
 # Radio (needed for audio controls even on wifi-only)
 PRODUCT_PACKAGES += \
-    libsecril-client \
-    libsecril-client-sap \
     libril \
     librilutils \
-    rild
+    rild \
+    modemloader
 
 # Recovery
 PRODUCT_PACKAGES += \
@@ -211,14 +210,6 @@ PRODUCT_PACKAGES += \
     hostapd_default.conf \
     libwpa_client \
     wpa_supplicant
-
-# Radio
-PRODUCT_PACKAGES += \
-    libril \
-    librilutils \
-    rild \
-    libxml2 \
-    libprotobuf-cpp-full
 
 PRODUCT_PACKAGES += \
     libnetcmdiface \
