00b333a5214a2cbd59f2d814b83de512f780332a
Sean Hoyt <seanhoyt963@gmail.com>
2016-12-10 22:14:26 -0600

    Fix ramdisk
    Change-Id: I3597e0976789fd7af07ec248d02d7dbd908289b9

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc
+LOCAL_SRC_FILES		:= init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -12,7 +12,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.baseband.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.baseband.rc
+LOCAL_SRC_FILES		:= init.baseband.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -20,6 +20,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5420.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/ueventd.universal5420.rc
+LOCAL_SRC_FILES		:= ueventd.universal5420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
