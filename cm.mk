2dec86f3566803eb3f804a34ee9e9c502914eea1
Thomas Patry <thompatry@gmail.com>
2016-12-13 19:54:28 -0600

    Fix a small derp in the cm.mk from ha3g
    Change-Id: Idc678664c395453e802ba87e994a3fe1a050b24d

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from ha3g device
-$(call inherit-product, device/samsung/ha3g/device.mk)
+$(call inherit-product, device/samsung/v1a3g/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
