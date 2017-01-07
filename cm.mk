9854ae98d7d5fa76774ea583a7e1bb56e9e385b7
Thomas Patry <thompatry@gmail.com>
2017-01-07 17:28:21 -0600

    Revert "Rebrand to LineageOS"
    Lineage is not rebranding the CM-13.0 branch.
    
    This reverts commit ff4828950832cbbde7bd8a174c12667d693781b6.
    
    Change-Id: Ia4fd483e9b18dba71951fdb86f9bde2e641e09aa

--- cm.mk
+++ cm.mk
@@ -6,13 +6,13 @@ $(call inherit-product, device/samsung/v1a3g/device.mk)
 
 PRODUCT_BRAND := samsung
 
-# Inherit some common LineageOS stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit more cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
-PRODUCT_NAME := lineage_v1a3g
+PRODUCT_NAME := cm_v1a3g
 PRODUCT_DEVICE := v1a3g
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
